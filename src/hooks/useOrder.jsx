65ee5d2e432395dde9cd1676c1d97ff54d002c60
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2024-07-07 16:26:32 -0600

    add function enviar pedido
    se agrego una funcion para enviar el pedido por whatsapp

--- src/hooks/useOrder.jsx
+++ src/hooks/useOrder.jsx
@@ -1,15 +1,12 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import toast from "react-hot-toast";
 
 const useOrder = () => {
   const body = document.querySelector('body');
   const [orden, setOrden] = useState([])
   const [active, setActive] = useState(false)
-  const [total, setTotal] = useState(0)
 
-
-
-
+  const totalPedido = useMemo(() => orden.reduce((total, item) => total + (item.cantidad * item.precio), 0), [orden])
 
   const addProducto = (item) => {
 
@@ -45,12 +42,36 @@ const useOrder = () => {
     }
   }
 
-  
+  const enviarPedido = () => {
+
+    const nombre = prompt('Cual es tu nombre ?')
+
+
+    try {
+      if (nombre === '' || typeof nombre === Number || nombre === null || nombre.length < 4) {
+        alert('Agrega un nombre')
+      } else {
+        const miPedido = orden.map(item => `- *${item.cantidad}* ${item.nombre} ($${item.precio}) - *$${item.cantidad * item.precio}*`).join('\n');
+        const mensaje = encodeURIComponent(`¡Hola me llamo *${nombre}*! y me gustaria encargar estos productos:\n\n${miPedido}\n\n Total: *$${totalPedido}*`)
+        const whatsappUrl = `https://wa.me/+524191389032?text=${mensaje}`
+
+        window.open(whatsappUrl)
+      }
+    } catch (error) {
+      toast(
+        "No se pudo enviar tu pedido.\n\nIntenta otra vez, evita bloquear las ventanas emergentes",
+        {
+          duration: 5000,
+        }
+      );
+    }
+
+  }
 
 
 
   return (
-    { addProducto, mostrarModal, active, orden, removeItem }
+    { addProducto, mostrarModal, active, orden, removeItem, enviarPedido }
   )
 }
 export default useOrder
